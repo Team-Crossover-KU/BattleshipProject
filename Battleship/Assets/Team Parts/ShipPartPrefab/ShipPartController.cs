5970ba3aeeaee072d75572a9d8f718abe3185c8c
Chase A Stump <chaseomega@gmail.com>
2019-09-14 21:29:46 -0500

    Added Flag to ShipPartController to store if hit

--- Battleship/Assets/Team Parts/ShipPartPrefab/ShipPartController.cs
+++ Battleship/Assets/Team Parts/ShipPartPrefab/ShipPartController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ShipPartController : MonoBehaviour
 {
     Collider2D collide;
+    bool hit = false;
 
     // Start is called before the first frame update
     void Start()
