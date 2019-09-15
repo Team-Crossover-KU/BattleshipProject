32c8fb89d2b4806971278bdba0540a95522f9ef6
Jarod Davis <jarod.davis37@gmail.com>
2019-09-14 22:36:57 -0500

    Added function stubs for ShipController

--- Battleship/Assets/Team Parts/ShipPrefab/ShipController.cs
+++ Battleship/Assets/Team Parts/ShipPrefab/ShipController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
+    bool allPartsDestroyed = false;
+    bool isSpawned = false;
+    List<ShipPartController> parts;
+    private int shipLength = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,41 @@ public class ShipController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //spawn
+        Spawn();
     }
+
+    //for n length, spawn ship parts 
+    void Spawn()
+    {
+        //for loop for spawning
+    }
+
+    //setter for ship length
+    public void SetShipLength(int n)
+    {
+
+    }
+
+    //Check 
+    void checkParts()
+    {
+
+    }
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
