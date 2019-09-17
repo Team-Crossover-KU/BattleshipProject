bdc39b926eedcc0229ed078b73efd5353377f5c8
Marco Borth <borthmarco@ku.edu>
2019-09-16 21:54:33 -0500

    UI Components SetActive methods created

--- Battleship/Assets/Scripts/CanvasScript.cs
+++ Battleship/Assets/Scripts/CanvasScript.cs
@@ -3,43 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-// MultiCamera info. can be found in Unity User Manual (2019.2) / Graphics / Graphics Overview / Cameras / Using more than one camera
 public class CanvasScript : MonoBehaviour
 {
-    public Camera firstPersonCamera, overheadCamera;
-
     public Dropdown shipSelector;
-    public Button confirmButton, startButton, returnButton, fireButton, quitButton;
-
-    // MultiCamera info.
-    public void ShowOverheadView()
-    {
-        firstPersonCamera.enabled = false;
-        overheadCamera.enabled = true;
-    }
-
-    // MultiCamera info.
-    public void ShowFirstPersonView()
-    {
-        firstPersonCamera.enabled = true;
-        overheadCamera.enabled = false;
-    }
+    public UnityEngine.UI.Button confirmButton, startButton, returnButton, pauseButton, resumeButton, quitButton, yesButton, noButton, replayButton, mainMenuButton;
+    public GameObject shipSelectorPanel, shipPlacementPanel, gameUIPanel, pauseMenu, gameOverMenu, confirmationPanel;
 
     // Start is called before the first frame update
     public void Start()
     {
-        overheadCamera = Camera.main;
-        ShowOverheadView();
+        GameReset();
+
+        // Main Menu Buttons Listener Events.
         shipSelector.onValueChanged.AddListener(SelectShips);
         confirmButton.onClick.AddListener(BeginShipPlacement);
 
+        // Ship Placement Menu Buttons Listener Events.
         returnButton.onClick.AddListener(BackToSettings);
         startButton.onClick.AddListener(StartGame);
 
+        // Game Button Listener Events.
+        pauseButton.onClick.AddListener(PauseGame);
+
+        // Pause Menu Button Listener Events.
+        resumeButton.onClick.AddListener(ResumeGame);
         quitButton.onClick.AddListener(QuitGame);
-        GameReset();
+
+        // Confirmation Menu Button Listener Events.
+        yesButton.onClick.AddListener(GameReset);
+        noButton.onClick.AddListener(PauseGame);
+
+        // Game Over Menu Button Listener Events.
+        //replayButton.onClick.AddListener(BeginShipPlacement);
+        //mainMenuButton.onClick.AddListener(QuitGame);
     }
 
+    // SelectShips method will set the confirmation button to be interactable for any selection other than the empty option.
     private void SelectShips(int value)
     {
         value = shipSelector.value;
@@ -55,63 +54,75 @@ public class CanvasScript : MonoBehaviour
         }
     }
 
+    // BeginShipPlacement method will disable the Ship Selection Menu and enable the Ship Placement Menu.
     private void BeginShipPlacement()
     {
         Debug.Log("You have clicked the Confirm button!");
-        Vector3 moveToPosition = new Vector3(0, 0, 0);
-        overheadCamera.transform.LookAt(moveToPosition);
-        float speed = 2f;
-        transform.position = Vector3.Lerp(transform.position, moveToPosition, speed);
 
-        shipSelector.interactable = false;
-        confirmButton.interactable = false;
-        startButton.interactable = true;
-        returnButton.interactable = true;
+        shipSelectorPanel.SetActive(false);
+        shipPlacementPanel.SetActive(true);
     }
 
+    // BackToSettings method will return the game back to the Ship Selection Menu.
     private void BackToSettings()
     {
         Debug.Log("You have clicked the Return button!");
-        Vector3 moveToPosition = new Vector3(0, 0, 0);
-        overheadCamera.transform.LookAt(moveToPosition);
-        float speed = 2f;
-        transform.position = Vector3.Lerp(transform.position, moveToPosition, speed);
 
-        shipSelector.interactable = true;
-        confirmButton.interactable = true;
-        startButton.interactable = false;
-        returnButton.interactable = false;
+        shipSelectorPanel.SetActive(true);
+        shipPlacementPanel.SetActive(false);
     }
 
+    // StartGame method will start the Battleship game once all ships have been placed.
     private void StartGame()
     {
-        Debug.Log("You have clicked the Start button!");
-        Vector3 moveToPosition = new Vector3(-20, 4, -805);
-        overheadCamera.transform.LookAt(moveToPosition);
-        float speed = 2f;
-        transform.position = Vector3.Lerp(transform.position, moveToPosition, speed);
+        Debug.Log("You have clicked the StartGame button!");
 
-        startButton.interactable = false;
-        returnButton.interactable = false;
-        fireButton.interactable = true;
-        quitButton.interactable = true;
+        shipPlacementPanel.SetActive(false);
+        gameUIPanel.SetActive(true);
     }
 
+    // PauseGame method will pull up the pause menu once the Pause button is clicked.
+    private void PauseGame()
+    {
+        Debug.Log("You have clicked the Pause button!");
+        gameUIPanel.SetActive(false);
+        pauseMenu.SetActive(true);
+        confirmationPanel.SetActive(false);
+        quitButton.interactable = true;
+        resumeButton.interactable = true;
+    }
+
+    // ResumeGame method will disable the Pause menu and return back to the game.
+    private void ResumeGame()
+    {
+        Debug.Log("You have clicked the Resume button!");
+        gameUIPanel.SetActive(true);
+        pauseMenu.SetActive(false);
+    }
+
+    // QuitGame method will end the game once the user provides confirmation.
     public void QuitGame()
     {
         Debug.Log("You have clicked the Quit button!");
-        GameReset();
+        confirmationPanel.SetActive(true);
+        quitButton.interactable = false;
+        resumeButton.interactable = false;
     }
 
+    // GameReset method will start/restart the game at the Ship Selection menu.
     public void GameReset()
     {
-        shipSelector.interactable = true;
         shipSelector.value = 0;
 
         confirmButton.interactable = false;
-        startButton.interactable = false;
-        returnButton.interactable = false;
-        fireButton.interactable = false;
-        quitButton.interactable = false;
+        quitButton.interactable = true;
+        resumeButton.interactable = true;
+
+        shipSelectorPanel.SetActive(true);
+        shipPlacementPanel.SetActive(false);
+        gameUIPanel.SetActive(false);
+        pauseMenu.SetActive(false);
+        gameOverMenu.SetActive(false);
+        confirmationPanel.SetActive(false);
     }
 }
---
+++ Battleship/Assets/Scripts/NewMonoBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// MultiCamera info. can be found in Unity User Manual (2019.2) / Graphics / Graphics Overview / Cameras / Using more than one camera
+public class NewMonoBehaviour : MonoBehaviour
+{
+    public Camera firstPersonCamera, overheadCamera;
+
+    // MultiCamera info.
+    public void ShowOverheadView()
+    {
+        firstPersonCamera.enabled = false;
+        overheadCamera.enabled = true;
+    }
+
+    // MultiCamera info.
+    public void ShowFirstPersonView()
+    {
+        firstPersonCamera.enabled = true;
+        overheadCamera.enabled = false;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        overheadCamera = Camera.main;
+        ShowOverheadView();
+    }
+
+    void CameraPositionTransform(int x, int y, int z)
+    {
+        Vector3 moveToPosition = new Vector3(x, y, z);
+        overheadCamera.transform.LookAt(moveToPosition);
+        float speed = 2f;
+        transform.position = Vector3.Lerp(transform.position, moveToPosition, speed);
+    }
+}
