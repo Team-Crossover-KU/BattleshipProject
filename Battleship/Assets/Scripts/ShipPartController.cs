c0ec93a244df3e13b296aeb687c1ab93886071f4
Jarod Davis <jarod.davis37@gmail.com>
2019-09-22 17:47:44 -0500

    Added all comment blocks for ShipPartController.cs

--- Battleship/Assets/Scripts/ShipPartController.cs
+++ Battleship/Assets/Scripts/ShipPartController.cs
@@ -65,15 +65,23 @@ public class ShipPartController : MonoBehaviour
     }
 
     /**
-    * 
+    * @pre Ship part has been hit
+    * @post Change ship part flag to true, call parent ship's check function
+    * @param None
+    * @return None
     */
     public void Hit()
     {
         hit = true;
         parent.hitCheck();
     }
-    
-    
+
+    /**
+     * @pre Mouse has been clicked
+     * @post Change parent ships isMoving status to true
+     * @param None
+     * @retrun None
+     */
     private void OnMouseDown()
     {
         if (!parent.shipReadyToPair)
@@ -84,6 +92,12 @@ public class ShipPartController : MonoBehaviour
 
     }
 
+    /**
+     * @pre Mouse has been released
+     * @post Attempt to bond parent ship to board
+     * @param None
+     * @return None
+     */
     private void OnMouseUp()
     {
         
@@ -91,11 +105,14 @@ public class ShipPartController : MonoBehaviour
         parent.AttemptBond();
         parent.isMoving = false;
     }
-    
+
 
     /**
-    * 
-    */
+     * @pre Colliders made contact
+     * @post Attempt to pair/bond colliders of ship part and board square
+     * @param Collider2D collision
+     * @return None
+     */
     private void OnTriggerEnter2D (Collider2D collision)
     {
         if (collision.tag == "Open")
@@ -109,6 +126,12 @@ public class ShipPartController : MonoBehaviour
 
     }
 
+    /**
+     * @pre Colliders are currently in contact
+     * @post Maintain bond between ship part and board square
+     * @param Collider2D collision
+     * @return None
+     */
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.tag == "Open")
@@ -121,6 +144,12 @@ public class ShipPartController : MonoBehaviour
         }
     }
 
+    /**
+     * @pre Colliders have left contact
+     * @post Remove pair and revert bonding to unbonded state
+     * @param Collider2D collision
+     * @return None
+     */
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.tag == "Open")
