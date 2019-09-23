8228f7d49b5a16c1bf9c96676b52e3860d3d3461
Rachel Elting <rachelelting@gmail.com>
2019-09-22 22:14:28 -0500

    Update ButtonController to be recognized by Doxygen.

--- Battleship/Assets/Scripts/buttonController.cs
+++ Battleship/Assets/Scripts/buttonController.cs
@@ -7,18 +7,30 @@ public class buttonController : MonoBehaviour
     public ShipPartController target;
     public Collider2D collide;
     public int buttonTeam;
-    // Start is called before the first frame update
-    void Start()
+
+    /**
+    * @pre Start is called before the first frame update.
+    * @post Checks the object the script is attached to. If one exists, attach to the collide variable.
+    * @param None.
+    * @return None.
+    */
+    private void Start()
     {
         collide = this.GetComponent<Collider2D>();
     }
 
-    // Update is called once per frame
+    // Update is called once per frame. Not used, so it's not documented.
     void Update()
     {
         
     }
 
+    /**
+    * @pre Target must be able to run Hit.
+    * @post Attemts to run the Hit function on the attached target.
+    * @param None.
+    * @return None.
+    */
     public void Onclickhit()
     {
         if (target != null)
@@ -29,6 +41,12 @@ public class buttonController : MonoBehaviour
         }
     }
 
+    /**
+    * @pre Object must have an attached trigger collider.
+    * @post If it comes in contact with the collider of a viable object, it assigns that to target.
+    * @param takes parameter collision, the collider of what it hits.
+    * @return None.
+    */
     private void OnTriggerStay2D(Collider2D collision)
     {
         Debug.Log("Something touching the board");
